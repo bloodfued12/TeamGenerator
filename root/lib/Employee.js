703a63378b109090263c6aa92cbfc17f5cac082b
bloodfued12 <benjamin.smith.ep@gmail.com>
2024-01-02 14:09:00 +0000

    Added classes and subclasses to project

--- root/lib/Employee.js
+++ root/lib/Employee.js
@@ -1,15 +1,15 @@
 // TODO: Write code to define and export the Employee class
 
-class Employee = {
-    name: "";
-    id: 0;
-    email: "";
-    getName: function() {   };
-    getId: function () {   };
-    getEmail: function () {   };
-    getRole: function () {   };
+class Employee {
+    constructor(name, id, email, getName, getId, getEmail, getRole) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.getName =  function() { console.log(`${name}`)};
+    this.getId =  function () { console.log(`${id}`)  };
+    this.getEmail =  function () {console.log(`${email}`)   };
+    this.getRole =  function () {console.log("Employee")};
+}
 };
 
-module.exports = {
-    Employee
-}
+module.exports = Employee;
