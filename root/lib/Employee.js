7299717552db825c691d7922fc07901bcea78986
bloodfued12 <benjamin.smith.ep@gmail.com>
2024-01-02 13:17:54 +0000

    Added the class and exported the module for use too

--- root/lib/Employee.js
+++ root/lib/Employee.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee = {
+    name: "";
+    id: 0;
+    email: "";
+    getName: function() {   };
+    getId: function () {   };
+    getEmail: function () {   };
+    getRole: function () {   };
+};
+
+module.exports = {
+    Employee
+}
